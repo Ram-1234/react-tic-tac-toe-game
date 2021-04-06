901f1ef2be64fdf2259acf8bd4f6532752232649
Ram-1234 <ramnayan699@gmail.com>
2021-04-06 22:04:59 +0530

    Updates

---
+++ precache-manifest.511a23beb346eee940e42b7dd9c6ad13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52af513ae7251316a189f97646bd3a02",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "4b2ea9cf6e8d9655f91d",
+    "url": "/tic-tac-toe/static/css/main.3fa2e9eb.chunk.css"
+  },
+  {
+    "revision": "63295ab0b6da86866292",
+    "url": "/tic-tac-toe/static/js/2.e29e504d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/tic-tac-toe/static/js/2.e29e504d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b2ea9cf6e8d9655f91d",
+    "url": "/tic-tac-toe/static/js/main.c16498e0.chunk.js"
+  },
+  {
+    "revision": "112fb18effd48187aa7d",
+    "url": "/tic-tac-toe/static/js/runtime-main.9fd45e69.js"
+  }
+]);
